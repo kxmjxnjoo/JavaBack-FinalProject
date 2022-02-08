61c71ffe6e01f3034dd8218dedfbde1363f360b3
Jinkyum Park <61900235+jinkyumpark@users.noreply.github.com>
2022-02-08 15:10:48 +0900

    Add files via upload

--- SEMI_PROJECT/java/com/ezen/dto/PostDto.java
+++ SEMI_PROJECT/java/com/ezen/dto/PostDto.java
@@ -1,5 +1,50 @@
 package com.ezen.dto;
 
-public class PostDto {
+import java.sql.Date;
 
+public class PostDto {
+	private int postNum;
+	private String img;
+	private String content;
+	private String address;
+	private String userid;
+	private Date create_date;
+	
+	
+	public int getPostNum() {
+		return postNum;
+	}
+	public void setPostNum(int postNum) {
+		this.postNum = postNum;
+	}
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public Date getCreate_date() {
+		return create_date;
+	}
+	public void setCreate_date(Date create_date) {
+		this.create_date = create_date;
+	}
 }
