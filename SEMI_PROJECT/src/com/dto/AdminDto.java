e0dbbf08c59b419a6436bbfd3c89dc83cffda5d9
박진겸 <jinpark1025@gmail.com>
2022-02-15 11:50:37 +0900

    파일 정리

---
+++ SEMI_PROJECT/src/com/dto/AdminDto.java
@@ -0,0 +1,41 @@
+package ezen.spring.dto;
+
+public class AdminDto {
+
+	private String adminid;
+	private String password;
+	private String name;
+	private String email;
+	private String phone;
+	
+	public String getAdminid() {
+		return adminid;
+	}
+	public void setAdminid(String adminid) {
+		this.adminid = adminid;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+}
