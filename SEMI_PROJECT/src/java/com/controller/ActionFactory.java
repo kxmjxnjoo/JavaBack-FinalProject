64eb0391ff26815155ec4a2749472ca9480fef4b
Jinkyum Park <jinpark1025@gmail.com>
2022-02-17 00:12:54 +0900

    ActionFactory 정리

--- SEMI_PROJECT/src/java/com/controller/ActionFactory.java
+++ SEMI_PROJECT/src/java/com/controller/ActionFactory.java
@@ -44,44 +44,44 @@ public class ActionFactory {
 	
 		Action ac = null;
 		System.out.println(command);
-		 if(command.equals("postuploadForm")) ac = new PostUploadFormAction();
-		 if(command.equals("postUpload")) ac = new PostUploadAction();
-		 if(command.equals("postDetail")) ac = new PostDetailAction();
-		 if(command.equals("addReply")) ac = new AddReplyAction();
-		 if(command.equals("checkLike")) ac = new CheckLikeAction();
-		 if(command.equals("checkReplyLike")) ac = new CheckReplyLikeAction();
-		 if(command.equals("editPost")) ac = new EditPostAction();
-		 if(command.equals("editPostForm")) ac = new EditPostFormAction();
-		 if(command.equals("deletePost")) ac = new DeletePostAction();
-		 if(command.equals("reportForm")) ac = new ReportFormAction();
-		 if(command.equals("postReport")) ac = new PostReportAction();
-		 if(command.equals("storyUploadForm")) ac = new StoryUploadFormAction();
-		 if(command.equals("storyUpload")) ac = new StoryUploadAction();
-		 if(command.equals("storyDetail")) ac = new StoryDetailAction();
-		 if(command.equals("checkStoryLike")) ac = new CheckStoryLikeAction();
-		 if(command.equals("followArrows")) ac = new FollowArrowsAction();
-		 if(command.equals("deleteStory")) ac = new DeleteStoryAction();
-		 if(command.equals("editStoryForm")) ac = new EditStoryFormAction();
-		 if(command.equals("editStory")) ac = new EditStoryAction();
-		 if(command.equals("deleteReply")) ac = new DeleteReplyAction();
-		 if(command.equals("selectPost")) ac = new SelectPostAction();
+		if(command.equals("postuploadForm")) 			ac = new PostUploadFormAction();
+		else if(command.equals("postUpload")) 			ac = new PostUploadAction();
+		else if(command.equals("postDetail")) 			ac = new PostDetailAction();
+		else if(command.equals("addReply")) 			ac = new AddReplyAction();
+		else if(command.equals("checkLike")) 			ac = new CheckLikeAction();
+		else if(command.equals("checkReplyLike")) 		ac = new CheckReplyLikeAction();
+		else if(command.equals("editPost")) 			ac = new EditPostAction();
+		else if(command.equals("editPostForm")) 		ac = new EditPostFormAction();
+		else if(command.equals("deletePost")) 			ac = new DeletePostAction();
+		else if(command.equals("reportForm")) 			ac = new ReportFormAction();
+		else if(command.equals("postReport")) 			ac = new PostReportAction();
+		else if(command.equals("storyUploadForm")) 		ac = new StoryUploadFormAction();
+		else if(command.equals("storyUpload")) 			ac = new StoryUploadAction();
+		else if(command.equals("storyDetail")) 			ac = new StoryDetailAction();
+		else if(command.equals("checkStoryLike")) 		ac = new CheckStoryLikeAction();
+		else if(command.equals("followArrows")) 		ac = new FollowArrowsAction();
+		else if(command.equals("deleteStory")) 			ac = new DeleteStoryAction();
+		else if(command.equals("editStoryForm")) 		ac = new EditStoryFormAction();
+		else if(command.equals("editStory")) 			ac = new EditStoryAction();
+		else if(command.equals("deleteReply")) 			ac = new DeleteReplyAction();
+		else if(command.equals("selectPost")) 			ac = new SelectPostAction();
 		 
-		if(command.equals("main"))				ac = new MainAction();
-		else if(command.equals("userpage"))		ac = new UserPageAction();
-		else if(command.equals("follow"))		ac = new FollowAction();
-		else if(command.equals("unfollow"))		ac = new UnfollowAction();
+		else if(command.equals("main"))					ac = new MainAction();
+		else if(command.equals("userpage"))				ac = new UserPageAction();
+		else if(command.equals("follow"))				ac = new FollowAction();
+		else if(command.equals("unfollow"))				ac = new UnfollowAction();
 		
-		else if(command.equals("likepost"))		ac = new LikePostAction();
-		else if(command.equals("unlikepost"))	ac = new UnlikePostAction();
+		else if(command.equals("likepost"))				ac = new LikePostAction();
+		else if(command.equals("unlikepost"))			ac = new UnlikePostAction();
 		
-		else if(command.equals("addreply"))		ac = new AddReplyAction();
+		else if(command.equals("addreply"))				ac = new AddReplyAction();
 		
-		else if(command.equals("notification"))	ac = new NotificationAction();
+		else if(command.equals("notification"))			ac = new NotificationAction();
 		
-		else if(command.equals("message"))		ac = new MessageAction();
+		else if(command.equals("message"))				ac = new MessageAction();
 		
-		else if(command.equals("search"))		ac = new SearchAction();
+		else if(command.equals("search"))				ac = new SearchAction();
 		 
-		 return ac;
+		return ac;
 	}
 }
