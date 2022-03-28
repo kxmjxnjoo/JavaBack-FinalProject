ef12378ae474ce8ad21f9aa14fa7ce0854753542
Jinkyum Park <jinpark1025@gmail.com>
2022-03-28 11:59:56 +0900

    initial spring starter project

---
+++ Springfeed/src/main/java/com/ezen/springfeed/SpringfeedApplication.java
@@ -0,0 +1,13 @@
+package com.ezen.springfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringfeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringfeedApplication.class, args);
+    }
+
+}
---
+++ Springfeed/src/test/java/com/ezen/springfeed/SpringfeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezen.springfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringfeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
