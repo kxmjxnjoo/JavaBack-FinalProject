7a6162a95249bc20c53500d06997076fe291ec1b
jinjoo <pearls0803@naver.com>
2022-03-28 13:33:32 +0900

    add anotation

--- Springfeed/src/main/java/com/ezen/springfeed/service/AdminService.java
+++ Springfeed/src/main/java/com/ezen/springfeed/service/AdminService.java
@@ -1,5 +1,13 @@
 package com.ezen.springfeed.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ezen.springfeed.dao.IAdminDao;
+
+@Service
 public class AdminService {
 
+	@Autowired
+	IAdminDao adao;
 }
