6a207372cc940a50e30fb36642aaf9e44e722963
Jinkyum Park <jinpark1025@gmail.com>
2022-03-28 13:21:58 +0900

    add member controller

--- Springfeed/src/main/java/com/ezen/springfeed/controller/MemberController.java
+++ Springfeed/src/main/java/com/ezen/springfeed/controller/MemberController.java
@@ -1,4 +1,54 @@
 package com.ezen.springfeed.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Controller
 public class MemberController {
+
+    // TODO : implement
+    @RequestMapping("/login")
+    public String login() {
+        return "";
+    }
+
+    // TODO : implement
+    @RequestMapping("/loginForm")
+    public String loginForm() {
+        return "";
+    }
+
+    @RequestMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        session.removeAttribute("loginUser");
+        return "main";
+    }
+
+    // TODO : implement
+    @RequestMapping("/join")
+    public String join() {
+        return "";
+    }
+
+    // TODO : implement
+    @RequestMapping("/joinForm")
+    public String joinForm() {
+        return "";
+    }
+
+    // TODO : implement
+    @RequestMapping("/follow")
+    public String follow() {
+        return "";
+    }
+
+    // TODO : implement
+    @RequestMapping("/unfollow")
+    public String unfollow() {
+        return "";
+    }
 }
