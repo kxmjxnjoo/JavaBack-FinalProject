a17ba616134dbc29547ed4ed4c4b7d0d05b66642
jinjoo <pearls0803@naver.com>
2022-03-28 14:26:34 +0900

    add something...

--- Springfeed/src/main/java/com/ezen/springfeed/controller/AdminController.java
+++ Springfeed/src/main/java/com/ezen/springfeed/controller/AdminController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import com.ezen.springfeed.service.AdminService;
+import com.ezen.springfeed.service.PostService;
 
 @Controller
 public class AdminController {
@@ -11,4 +12,6 @@ public class AdminController {
 	@Autowired
 	AdminService as;
 
+	@Autowired
+	PostService ps;
 }
