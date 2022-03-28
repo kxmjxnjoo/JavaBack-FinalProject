f66d3b843fde6f2ee9ac67c9a4775317c7b411b5
Jinkyum Park <jinpark1025@gmail.com>
2022-03-28 14:22:50 +0900

    add wallet.zip

--- Springfeed/src/main/java/com/ezen/springfeed/controller/AdminController.java
+++ Springfeed/src/main/java/com/ezen/springfeed/controller/AdminController.java
@@ -4,11 +4,45 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import com.ezen.springfeed.service.AdminService;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AdminController {
 	
 	@Autowired
 	AdminService as;
+
+	// TODO : implement
+	@RequestMapping("/admin/login")
+	public String adminLogin() {
+		return "";
+	}
+
+	// TODO : implement
+	@RequestMapping("/admin/loginForm")
+	public String adminLoginForm() {
+		return "";
+	}
+
+	// TODO : implement
+	@RequestMapping("/admin/memberList")
+	public String memberList() {
+		return "";
+	}
+
+	// TODO : implement
+	@RequestMapping("/admin/searchMember")
+	public String searchMember() {
+		return "";
+	}
+
+	// TODO : implement
+	@RequestMapping("/admin/report")
+	public String adminReport() {
+		return "";
+	}
+
+	// TODO : implement
+
 
 }
