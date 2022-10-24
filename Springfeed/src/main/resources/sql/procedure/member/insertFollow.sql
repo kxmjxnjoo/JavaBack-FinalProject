0e4069abb77eb39441643a472d3dd764f2fccade
jinkyumpark <jinpark1025@gmail.com>
2022-10-24 20:34:28 +0900

    relocate sql files

--- Springfeed/src/main/resources/sql/procedure/member/insertFollow.sql
+++
@@ -1,9 +0,0 @@
-create or replace procedure insertFollow(
-    p_followed in member.userid%TYPE,
-    p_follower in member.userid%TYPE
-)
-is
-begin
-insert into follow (followed, follower) values (p_followed, p_follower);
-commit;
-end;
